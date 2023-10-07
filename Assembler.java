9711dd4a95b93be272496f08c3e751c73d56485c
panthaweekan_somngam <tatanoobza1234@hotmail.com>
2023-10-07 16:13:00 +0700

    Optimization Assembler and Fix bug

--- Assembler.java
+++ Assembler.java
@@ -1,64 +1,64 @@
 import java.math.BigInteger;
 import java.util.*;
-import java.util.regex.Pattern;
 import static java.lang.Integer.toBinaryString;
 
 public class Assembler {
 
-    /** Important parameters **/
-    boolean TestTool = true;
-    private static final String zero_bit = "0000000";   // 31-25 (7 bit) must be 0 bit
-    private static final Pattern number_pattern = Pattern.compile("-?\\d+(\\.\\d+)?");
+    // Assembler constructor
     private final AssemblerTokenizer tkz;
     Assembler(String assembly) {
         tkz = new AssemblerTokenizer(assembly);
     }
+
+    /** Implement parameters **/
+    boolean TestTool = true;
+    private static final String zero_bit = "0000000";   // 31-25 (7 bit) must be 0 bit (from Documentation)
     private final List<String> machineCodes = new ArrayList<>();
     private String machineCode = zero_bit;              // add zero-bit first before load
-    private int curr_Line = 0;
-    public List<String> Data_list = new ArrayList<>(); // to collect parsed line
+    private int pointer = 0;                            // for count line by line
+    public List<String> Data_list = new ArrayList<>();  // to collect parsed line
     private final Map<String, Integer> Label_Mapping = new HashMap<>();
+    public static String getOpcode(String instruction) {
+        return switch (instruction) {
+            case "add"      -> "000";
+            case "nand"     -> "001";
+            case "lw"       -> "010";
+            case "sw"       -> "011";
+            case "beq"      -> "100";
+            case "jalr"     -> "101";
+            case "halt"     -> "110";
+            case "noop"     -> "111";
+            case ".fill"    -> "no-opcode";
+            default         -> null;
+        };
+    }
 
-    private static final Map<String , String> Opcode_mapping = Map.of(
-            "add"  , "000",
-            "nand" , "001",
-            "lw" , "010",
-            "sw" , "011",
-            "beq" , "100",
-            "jalr" , "101",
-            "halt" , "110",
-            "noop" , "111",
-            ".fill" , ".fi"
-    );
-    private static final Map<String , String> Instruction_mapping = Map.of(
-            "add"  , "R",
-            "nand" , "R",
-            "lw" , "I",
-            "sw" , "I",
-            "beq" , "I",
-            "jalr" , "J",
-            "halt" , "O",
-            "noop" , "O",
-            ".fill", "F"
-    );
-    private static final Map<String , Integer> FieldNum = Map.of(
-            "add"  , 3,
-            "nand" , 3,
-            "lw" , 2,
-            "sw" , 2,
-            "beq" , 2,
-            "jalr" , 2,
-            "halt" , 0,
-            "noop" , 0,
-            ".fill", 0
-    );
+    public static String getType(String instruction) {
+        return switch (instruction) {
+            case "add", "nand"      -> "R";
+            case "lw", "sw", "beq"  -> "I";
+            case "jalr"             -> "J";
+            case "halt", "noop"     -> "O";
+            case ".fill"            -> "F";
+            default                 -> null;
+        };
+    }
+
+    public static int getNumberOfField(String instruction) {
+        return switch (instruction) {
+            case "add", "nand"              -> 3;
+            case "lw", "sw", "beq", "jalr"  -> 2;
+            case "halt", "noop", ".fill"    -> 0;
+            default                         -> 0;
+        };
+    }
 
     /** Parser **/
     void parseLineToData(){
         Data_list = new ArrayList<>();
         while (tkz.hasNext()) {         // parse to data until newline
             String token = tkz.next();
-            if (token.equals("\n")) {  // new line
+            if (token.equals("\n")) {   // new line
                 break;
             }
             Data_list.add(token);
@@ -70,20 +70,19 @@ public class Assembler {
         }
     }
 
-
     /**
      *  Main machineCode Encoder
      */
     public List<String> computeToMachineCode(){
         System.out.println("*** Start mapping Label before compute! ***");
-        LabelMapping();               // parse all line to get label and put to Label_Mapping
+        LabelMapping();                 // parse all line to get label and put to Label_Mapping
         if(TestTool) System.out.println("Label Mapping : " + Label_Mapping);
-        return(compute());          // get MachineCode by instruction-type
+        return(compute());              // get MachineCode by instruction-type
     }
 
     public void reset(){
-        tkz.repositionToStart();    // reposition to start token!!!
-        curr_Line = 0;
+        tkz.repositionToStart();        // reposition to start token!!!
+        pointer = 0;
     }
 
     private ArrayList<String> compute() {
@@ -91,10 +90,10 @@ public class Assembler {
         parseLineToData();
         while (tkz.hasNext()) {
             int index = 0;
-            if(TestTool) System.out.println("Line : " + (curr_Line + 1));
+            if(TestTool) System.out.println("Line : " + (pointer + 1));
+
             if (Data_list.isEmpty()) {
                 parseLineToData();
-                continue;
             }
 
             /**
@@ -113,40 +112,44 @@ public class Assembler {
                 }
             }
 
-
             /**
              * get instruction from Data_list[index]
              * get type and opcode from instruction mapping
              */
             String instruction = Data_list.get(index);
-            String type = Instruction_mapping.get(instruction);
-            String opcode = Opcode_mapping.get(instruction);
+            String type = getType(instruction);
+            String opcode = getOpcode(instruction);
 
-            if(TestTool){
+            if(TestTool){   // print all params
                 System.out.println("Instruction : " + instruction);
                 System.out.println("Type : " + type);
                 System.out.println("Opcode : " + opcode);
             }
 
-
             /**
              * set Starter-package
              */
-            int FieldNum = Assembler.FieldNum.get(instruction);
-            String[] fields = {"", "", ""};
+            int FieldNum = getNumberOfField(instruction);
+            String[] fields = new String[3];
+
+            // add zero-bit with opcode first
             machineCode = zero_bit;
             machineCode += opcode;
 
+            // for instruction that have a Field Number
             for (int i = 0; i < FieldNum; ++i) {
                 fields[i] = Data_list.get(index + 1 + i);
 
+                // check isInt
                 if (!isInteger(fields[i])) {
                     System.out.println("Fields -> " + i + " is not an integer.");
                     exit(1);
                 }
 
+                // turn to int
                 int integerFields = toInteger(fields[i]);
 
+                // check int value
                 if (integerFields < 0 || integerFields > 7) {
                     System.out.println("Fields -> " + i + " is out of [0,7]");
                     exit(1);
@@ -163,14 +166,18 @@ public class Assembler {
              */
             switch (type) {
                 case "R" -> R_type(fields);                          // finished
-                case "I" -> I_type(fields,index,instruction);        // in-progress
-                case "J" -> J_type(fields);                          // in-progress
+                case "I" -> I_type(fields,index,instruction);        // finished
+                case "J" -> J_type(fields);                          // finished
                 case "O" -> O_type();                                // finished
-                case "F" -> F_type(index);                           // in-progress
+                case "F" -> F_type(index);                           // finished
+                default -> {
+                    System.out.println("No type matche");
+                    exit(1);
+                }
             }
 
             machineCodes.add(machineCode);      // add to machineCode-list
-            curr_Line++;
+            pointer++;
             parseLineToData();
         }
         if(TestTool) System.out.println("------------------------------------------------------------------------------");
@@ -194,7 +201,7 @@ public class Assembler {
 
         if(isLabel(fields[2])){
                                                    // address = PC + 1 + offset
-            if(Objects.equals(instruction, "beq")) offset = Label_Mapping.get(fields[2]) - curr_Line - 1;
+            if(Objects.equals(instruction, "beq")) offset = Label_Mapping.get(fields[2]) - pointer - 1;
             else offset = Label_Mapping.get(fields[2]);    // lw & sw
 
         } else if(isInteger(fields[2])){
@@ -233,18 +240,17 @@ public class Assembler {
             machineCode += "0000000000000000000000";
     }
 
-    private void F_type(int index){ // no field
-        // not-implement yet
+    private void F_type(int index){     // no field
         machineCode = "";
         String value = Data_list.get(1 + index); // next field value
 
-        if(isInteger(value)){   // Integer State
+        if(isInteger(value)){       // Integer State
             int integer = toInteger(value);
             String binary;
             if(integer >= 0) {
                 binary = toBinaryString(integer);
                 machineCode = addZeroBits(binary,32);
-            } else {            // value < 0 then need to do 2's compliment!!!
+            } else {                // value < 0 then need to do 2's compliment!!!
                 binary = toBinaryString(-integer);
                 binary = addZeroBits(binary,32);
                 machineCode = twosCompliment(binary);
@@ -304,13 +310,13 @@ public class Assembler {
             }
             if(!isInstruction(Data_list.get(0))){
                 if(LabelValidCheck(Data_list.get(0))){
-                    Label_Mapping.put(Data_list.get(0) , curr_Line);
+                    Label_Mapping.put(Data_list.get(0) , pointer);
                 } else {
                     System.out.println("First index is not Label or instruction");
                     exit(1);
                 }
             }
-            curr_Line++;
+            pointer++;
             parseLineToData();
         }
     }
@@ -320,14 +326,14 @@ public class Assembler {
     }
 
     public static boolean isInstruction(String tkz){
-        return Instruction_mapping.containsKey(tkz);
+        return getType(tkz) != null;
     }
 
-    public static boolean isInteger(String field) {
-        if(number_pattern.matcher(field).matches()){
-            double number = Double.parseDouble(field);
-            return number % 1 == 0;
-        }else{
+    public static boolean isInteger(String input) {
+        try {
+            Integer.parseInt(input);
+            return true;
+        } catch (NumberFormatException e) {
             return false;
         }
     }
@@ -389,11 +395,11 @@ public class Assembler {
         return binaryStrings;
     }
 
-    /*
+     /**
         exit(i)
         i = 1 -> error
         i = 0 -> complete
-     */
+     **/
     public void exit(int i){
         System.out.println("exit (" + i + ") : ERROR!!!");
         System.exit(i);
